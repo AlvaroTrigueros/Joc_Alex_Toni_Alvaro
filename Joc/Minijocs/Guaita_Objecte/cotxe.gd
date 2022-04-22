7ad27230b6c495e9e3e6956a2f533b9c7354d770
tescudec <101245603+tescudec@users.noreply.github.com>
2022-04-22 10:53:29 +0200

    Funcionament cotxe, descàrrega d'objectes

--- Joc/Minijocs/Guaita_Objecte/cotxe.gd
+++ Joc/Minijocs/Guaita_Objecte/cotxe.gd
@@ -1,18 +1,20 @@
-extends AnimatedSprite
+extends Sprite
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+#var velocitat = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position.x += 5
-	if position.x > 1800:
-		position.x = -1600
-		
+func _process(_delta):
+	if Input.is_action_pressed("FLETXA_AMUNT") and position.y >= 100:
+		position.y -= 7
+	if Input.is_action_pressed("FLETXA_AVALL") and position.y <= 500:
+		position.y += 7
+	
