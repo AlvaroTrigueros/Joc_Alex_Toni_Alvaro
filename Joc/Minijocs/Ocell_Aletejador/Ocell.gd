2eefd0702ad4140500a186c1265d31249eb6a53c
juliaalarrosa <101245468+AlvaroTrigueros@users.noreply.github.com>
2022-05-02 12:28:52 +0200

    update 2.3.1

--- Joc/Minijocs/Ocell_Aletejador/Ocell.gd
+++ Joc/Minijocs/Ocell_Aletejador/Ocell.gd
@@ -7,6 +7,11 @@ extends KinematicBody2D
 var velocitat_ocell = Vector2(0 ,0)
 var salt = -350
 var gravetat = 1000
+var limit_velocitat_ocell = 525
+
+signal ocell_aletejador_game_over
+var viu = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,30 +20,36 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocitat_ocell.y += gravetat * delta
+	if velocitat_ocell.y < limit_velocitat_ocell:
+		velocitat_ocell.y += gravetat * delta
+		
 	if Input.is_action_just_pressed("ESPAI"):
 		if velocitat_ocell.y > 0:
 			velocitat_ocell.y = salt
-		else:
-			if velocitat_ocell.y > -500:
+			
+		elif velocitat_ocell.y > -limit_velocitat_ocell:
+			if velocitat_ocell.y + salt <= -limit_velocitat_ocell:
+				velocitat_ocell.y = -limit_velocitat_ocell
+			else:
 				velocitat_ocell.y += salt
-			else: 
-				pass
-				
+	if not viu:
+		ocell_aletejador_game_over()
+		
 	move_and_slide(velocitat_ocell)
 
 func ocell_aletejador_game_over():
 	gravetat = 0
 	velocitat_ocell.y = 0
 	salt = 0
+	emit_signal('ocell_aletejador_game_over')
 	$AnimatedSprite.stop()
 
 func _on_Canyeria_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body.name == 'Ocell':
-		ocell_aletejador_game_over()
+		viu = false
 
 
 func _on_Terra_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body.name == 'Ocell':
-		ocell_aletejador_game_over()
+		viu = false
 
--- Joc/Minijocs/Ocell_Aletejador/Ocell_Aletejador.gd
+++ Joc/Minijocs/Ocell_Aletejador/Ocell_Aletejador.gd
@@ -7,38 +7,40 @@ var troba_nova_altura = true
 var puntuacio_ocell_aletejador = 0
 var limit_genera_canyeria = 200
 
+onready var file = 'res://Minijocs/Ocell_Aletejador/high_score_ocell_aletejador.txt'
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_file(file)
+	
 	$Ocell.velocitat_ocell = Vector2(0 ,0)
 	$Ocell.salt = -350
 	$Ocell.gravetat = 1000
+	
 	$Canyeria1.velocitat_canyeria = 200
 	$Canyeria2.velocitat_canyeria = 200
 	$Canyeria3.velocitat_canyeria = 200
+	
+	$Pantalla_mort.visible = false
 	
 	altura_canyeria = $Canyeria3.position.y
 	
 	$Marcador.text = str(puntuacio_ocell_aletejador)
 	
+	$Fons1.velocitat_fons = -100
+	$Fons2.velocitat_fons = -100
 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#$Marcador.rect_size = $Marcador.get_font("font").get_string_size($Marcador.text)
+	if $Canyeria1.velocitat_canyeria > 0.1:
+		$Canyeria1.velocitat_canyeria += 2.5 * delta
+		$Canyeria2.velocitat_canyeria += 2.5 * delta
+		$Canyeria3.velocitat_canyeria += 2.5 * delta
 	
-	$Canyeria1.velocitat_canyeria += 2.5 * delta
-	$Canyeria2.velocitat_canyeria += 2.5 * delta
-	$Canyeria3.velocitat_canyeria += 2.5 * delta
-	
-		
-	if $Ocell.gravetat == 0:
-		$Canyeria1.velocitat_canyeria = 0
-		$Canyeria2.velocitat_canyeria = 0
-		$Canyeria3.velocitat_canyeria = 0
-		
-		if Input.is_action_just_pressed("ESPAI"):
-			get_tree().reload_current_scene()
-			
-	
+
 	while troba_nova_altura:
 		altura_canyeria = rand_range((altura_canyeria)+limit_genera_canyeria, (altura_canyeria)-limit_genera_canyeria)
 		if altura_canyeria > 99 and altura_canyeria < 516:
@@ -78,3 +80,34 @@ func _on_Canyeria3_puntua():
 	$Marcador.text = str(puntuacio_ocell_aletejador)
 
 
+
+
+func _on_Ocell_ocell_aletejador_game_over():
+	
+	$Canyeria1.velocitat_canyeria = 0
+	$Canyeria2.velocitat_canyeria = 0
+	$Canyeria3.velocitat_canyeria = 0
+	
+	$Fons1.velocitat_fons = 0
+	$Fons2.velocitat_fons = 0
+	
+	$Pantalla_mort.visible = true
+	
+	if Input.is_action_just_pressed("ESPAI"):
+		$Ocell.viu = true
+		get_tree().reload_current_scene()
+		
+
+func load_file(file_path):
+	#https://godotengine.org/qa/57130/how-to-import-and-read-text
+	var f = File.new()
+	f.open(file, File.READ)
+	var index = 1
+	while not f.eof_reached(): # iterate through all lines until the end of file is reached
+		var line = f.get_line()
+		line += " "
+		print(line + str(index))
+
+		index += 1
+	f.close()
+	return
