c61da3fda0147fd2dff108376badf23807766c66
AlvaroTrigueros <101245468+AlvaroTrigueros@users.noreply.github.com>
2022-05-15 22:42:01 +0200

    actualización tiro a la lata
    minijuego tiro a la lata casi acabado

--- Joc/Minijocs/Tir_a_la_llauna/Escenes_llaunes/Llauna.gd
+++ Joc/Minijocs/Tir_a_la_llauna/Escenes_llaunes/Llauna.gd
@@ -2,23 +2,95 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-var velocitat_llauna = Vector2.ZERO
-var Po = Vector2(0, 650)
-var escala = 1
+var velocitat_llauna = null
+#var Po = Vector2(0, 650)
+var escala = 1.0
 var gravetat = 350
+var Va = 0
+var hit = false
+var destroy = 0
+var canvia_sentit = 1
+
+signal puntua
+signal resta_vida
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	
+	position = Vector2(rand_range(0+200, 1024-200), 650)
+	
 	if rand_range(1, 2) < 1.3:
 		escala = rand_range(0.5, 1)
-		
-	velocitat_llauna.x = rand_range(-100, 100)
-	velocitat_llauna.y = rand_range(, 100)
-	scale = escala
+	
+	Va = rand_range(-30, 30)
+	
+	
+	#FALTA HACER QUE SI SPAWNEAN A UN LADO NO VAYAN MUY RAPIDAS EN EL MISMO SENTIDO
+	velocitat_llauna = genera_velocitat_llauna()
+	
+	
+	scale = Vector2(escala, escala)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	velocitat_llauna.y += gravetat*delta
+	
+	
+	if hit:
+		destroy += 1*delta
+		
+	if destroy > 0.05:
+		scale *= 0.97
+		#FALTA HACER QUE CUANDO SE LE DÉ VAYA AL FONDO, POR DETRÁS DE LAS OTRAS LATAS, PERO NO DEL FONDO
+		
+	
+	if scale.x < 0.01:
+		queue_free()
+	
+	if (position.y > 700 or position.x < 0-50 or position.x > 1024+50) and not hit:
+		emit_signal('resta_vida')
+		queue_free()
+		
+	rotation_degrees += Va*delta
+	
+	position.x += velocitat_llauna.x*delta
+	position.y += velocitat_llauna.y*delta
+
+
+func _on_Area_llauna_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		hit = true
+		
+		canvia_sentit = (randi()%2)
+		#print(canvia_sentit)
+		if canvia_sentit == 0:
+			canvia_sentit = -1
+			
+		Va *= canvia_sentit * rand_range(3, 10)
+		velocitat_llauna.x *= canvia_sentit
+		
+		emit_signal('puntua')
+
+
+func genera_velocitat_llauna():
+	var velocitat_llauna = Vector2.ZERO
+	
+	velocitat_llauna.y = rand_range(-650, -450)
+	
+	var troba_nova_velocitat = true
+	
+	while troba_nova_velocitat:
+		randomize()
+		velocitat_llauna.x = rand_range(-300, 300)
+			
+		if (position.x < 0+250 and velocitat_llauna.x < -150) or (position.x > 1024-250 and velocitat_llauna.x > 150):
+			pass
+			
+		else:
+			break
+	
+	return velocitat_llauna
--- Joc/Minijocs/Tir_a_la_llauna/Tir_a_la_llauna.gd
+++ Joc/Minijocs/Tir_a_la_llauna/Tir_a_la_llauna.gd
@@ -2,15 +2,110 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var puntuacio_tir_a_la_llauna = 0
+var tirs = 0
+var precisio = 0
+var vides_tir_a_la_llauna = 3
+
+var reduccio_temps = 0
+var minim_temps = 0
+var temps_entre_llaunes = 2
+
+var timing_llaunes = null
+
+
+var Llauna_pepsico = preload('res://Minijocs/Tir_a_la_llauna/Escenes_llaunes/Llauna_pepsico.tscn')
+var Llauna_fakecola = preload('res://Minijocs/Tir_a_la_llauna/Escenes_llaunes/Llauna_fakecola.tscn')
+var Llauna_fantacherry = preload('res://Minijocs/Tir_a_la_llauna/Escenes_llaunes/Llauna_fantacherry.tscn')
+var Llauna_llimonada = preload('res://Minijocs/Tir_a_la_llauna/Escenes_llaunes/Llauna_limonada.tscn')
+
+
+var choices = [Llauna_pepsico, Llauna_fakecola, Llauna_fantacherry , Llauna_llimonada]
+var nova_llauna = null
+
+
+var genera_nova_llauna = 0
+
+
+onready var animacio = $YSort/Pistola/AnimacioPistola
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-
+	timing_llaunes = genera_temps_entre_llaunes()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if vides_tir_a_la_llauna <= 0:
+		get_tree().reload_current_scene()
+		
+	if Input.is_action_just_pressed("click_esquerra"):
+		
+		animacio.frame = 0
+		
+		#frames 1, 2 y 3 de (0 de 18) de la animacion  eliminados para que vaya más rápido
+		animacio.play('dispara')
+		tirs += 1
+	
+	$Marcador.text = str(puntuacio_tir_a_la_llauna)
+	
+	precisio = calcula_precisio()
+	$Precisio.text = str(precisio) + '%'
+	
+	
+	genera_nova_llauna += 1*delta
+	
+	if genera_nova_llauna > timing_llaunes[0]:
+		#print(timing_llaunes[0])
+		timing_llaunes.erase(timing_llaunes[0])
+		
+		genera_nova_llauna = 0
+		
+		var choices = nova_llista_llaunes()
+		nova_llauna = choices[randi() % 4]
+		
+		nova_llauna.connect('puntua', self, '_on_KinematicBody2D_puntua')
+		nova_llauna.connect('resta_vida', self, '_on_KinematicBody2D_resta_vida')
+		
+		$YSort.add_child(nova_llauna)
+	
+	
+	
+func nova_llista_llaunes():
+	return [Llauna_pepsico.instance(), Llauna_fakecola.instance(), Llauna_fantacherry.instance(), Llauna_llimonada.instance()]
+
+
+func _on_KinematicBody2D_puntua():
+	puntuacio_tir_a_la_llauna += 1
+	
+	
+func _on_KinematicBody2D_resta_vida():
+	vides_tir_a_la_llauna -= 1
+	
+	
+func round_to_dec(num, digit):
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
+
+
+func calcula_precisio():
+	if tirs == 0:
+		return 0
+	return round_to_dec(float(puntuacio_tir_a_la_llauna)/tirs * 100, 2)
+
+
+func genera_temps_entre_llaunes():
+	var lista = []
+	for i in range(0,1000):
+		randomize()
+		temps_entre_llaunes = rand_range(minim_temps, 3-reduccio_temps)
+		
+		if temps_entre_llaunes < 0.3:
+			minim_temps = 1.5-reduccio_temps/2
+		else:
+			minim_temps = 0
+		
+		reduccio_temps += 0.01
+		
+		lista.append(temps_entre_llaunes)
+	#print(lista)
+	return lista
